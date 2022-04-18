139f2b4cc03ae2964ba77aab86133f706b953ff9
Ken Xiong <kx2175@columbia.edu>
2022-04-18 11:09:10 -0400

    finish naming

--- static/quiz_naming.js
+++ static/quiz_naming.js
@@ -16,14 +16,14 @@ function displaySelect(name) {
     for (var i = 0; i < name.length; i++){
         let new_select = $("<select id = s_"+i+">")
         for (var j = 0; j < name.length; j++){
-            var opt = new Option(name[j], j);
-            opt.value = j;
-            opt.text = name[j];
-            new_select.append(opt);
+            var opt = new Option(name[j], j)
+            opt.value = j
+            opt.text = name[j]
+            new_select.append(opt)
         }
         let new_col = $("<div class='col-md-2' id='p_" + i + "'>");
-        new_col.append(new_select);
-        $("#select_container").append(new_col);
+        new_col.append(new_select)
+        $("#select_container").append(new_col)
     }
 }
 
@@ -32,13 +32,25 @@ function check(name){
     for (var i = 0; i < name.length; i++){
         var e = document.getElementById("s_"+i)
         var text = e.options[e.selectedIndex].text
-        console.log(text)
         if (name[i] == text){
-            correct.push(1)
+            correct.push(i)
+            $("#s_"+i).css({"background-color": "green"})
+        }else{
+            $("#s_"+i).css({"background-color": "red"})
         }
         $("#s_"+i).prop('disabled', true)
     }
-    console.log(correct)
+    $("#dialog").dialog({
+          buttons: [
+          {
+                text:"You got "+correct.length+" right",
+                click: function() {
+                $( this ).dialog( "close" );
+            }
+          }
+          ]
+    });
+
 
     $.ajax({
         type: "POST",
